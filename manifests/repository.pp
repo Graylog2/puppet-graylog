ecb4d40d01b74a9fac40d04cf2e9e6ee74a2aac8
K2patel <k2patel@users.noreply.github.com>
2017-03-03 20:03:09 -0500

    Trying to fix issue with puppet-lint report
    can not figure out what does this mean.
    
    manifests/repository.pp - WARNING: top-scope variable being used without an explicit namespace on line 12

--- manifests/repository.pp
+++ manifests/repository.pp
@@ -7,26 +7,26 @@ class graylog::repository(
   anchor { 'graylog::repository::end': }
   
   if $url == undef {
-    $repo_url = $::osfamily ? {
+    $graylog_repo_url = $::osfamily ? {
       'debian' => 'https://downloads.graylog.org/repo/debian/',
-      'redhat' => "https://downloads.graylog.org/repo/el/${repository_release}/${version}/\$basearch/",
+      'redhat' => "https://downloads.graylog.org/repo/el/${release}/${version}/\$basearch/",
       default  => fail("${::osfamily} is not supported!"),
       }
   } else {
-    $repo_url = $url
+    $graylog_repo_url = $url
   }
 
   case $::osfamily {
     'debian': {
       class { 'graylog::repository::apt':
-        url     => $repo_url,
+        url     => $graylog_repo_url,
         release => $release,
         version => $version,
       }
     }
     'redhat': {
       class { 'graylog::repository::yum':
-        url => $repo_url,
+        url => $graylog_repo_url,
       }
     }
     default: {
