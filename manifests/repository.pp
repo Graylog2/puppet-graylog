4e869fdb231b4a2ce113402a6606e68e088b16a5
K2patel <k2patel@users.noreply.github.com>
2017-03-03 19:36:07 -0500

    Fixing issue #10 for not propagating major_versoin
    Puppet can not rebuild URL which is already built.

--- manifests/repository.pp
+++ manifests/repository.pp
@@ -1,22 +1,32 @@
 class graylog::repository(
   $version = $graylog::params::major_version,
-  $url     = $graylog::params::repository_url,
+  $url     = undef,
   $release = $graylog::params::repository_release,
 ) inherits graylog::params {
   anchor { 'graylog::repository::begin': }
   anchor { 'graylog::repository::end': }
 
+  if $url == undef {
+     $repo_url = $::osfamily ? {
+       'debian' => 'https://downloads.graylog.org/repo/debian/',
+       'redhat' => "https://downloads.graylog.org/repo/el/${repository_release}/${version}/\$basearch/",
+       default  => fail("${::osfamily} is not supported!"),
+     }
+  } else {
+    $repo_url = $url
+  }
+
   case $::osfamily {
     'debian': {
       class { 'graylog::repository::apt':
-        url     => $url,
+        url     => $repo_url,
         release => $release,
         version => $version,
       }
     }
     'redhat': {
       class { 'graylog::repository::yum':
-        url => $url,
+        url => $repo_url,
       }
     }
     default: {
