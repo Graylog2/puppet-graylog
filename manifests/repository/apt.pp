1c64edb7b32e67da6f99b3a887b9fe1bc5b1294e
jsimpso <jsimpso@users.noreply.github.com>
2018-02-07 09:18:42 +0800

    Implemented proxy configuration

--- manifests/repository/apt.pp
+++ manifests/repository/apt.pp
@@ -2,9 +2,15 @@ class graylog::repository::apt(
   $url,
   $release,
   $version,
+  $proxy,
 ) {
   $apt_transport_package = 'apt-transport-https'
   $gpg_file = '/etc/apt/trusted.gpg.d/graylog-keyring.gpg'
+  $package_sources = [
+    'downloads.graylog.org',
+    'graylog-downloads.herokuapp.com',
+    'graylog2-package-repository.s3.amazonaws.com',
+  ]
 
   if !defined(Package[$apt_transport_package]) {
     ensure_packages([$apt_transport_package])
@@ -33,5 +39,42 @@ class graylog::repository::apt(
       Package[$apt_transport_package],
     ],
     notify   => Exec['apt_update'],
+  }
+
+  if $proxy {
+    file {'/etc/apt/apt.conf.d/01proxy':
+      ensure => present,
+    }
+    # Each file_line resource will append http or https proxy info for its specified source into the 01proxy file.
+    # If a line already exists for that source, and doesn't match the provided proxy param, that line will be replaced.
+    # If a line does not exist for that source, the provided info will be appended.
+    $package_sources.each |String $source| {
+      file_line { "Acquire::http::proxy::$source":
+        path    => '/etc/apt/apt.conf.d/01proxy',
+        match   => "Acquire::http::proxy::$source",
+        line    => "Acquire::http::proxy::$source \"$proxy\";",
+        require => File['/etc/apt/apt.conf.d/01proxy'],
+        before  => Apt::Source['graylog']
+      }
+      file_line { "Acquire::https::proxy::$source":
+        path    => '/etc/apt/apt.conf.d/01proxy',
+        match   => "Acquire::https::proxy::$source",
+        line    => "Acquire::https::proxy::$source \"$proxy\";",
+        require => File['/etc/apt/apt.conf.d/01proxy'],
+        before  => Apt::Source['graylog']
+      }
+    }
+  }
+  else {
+    # If proxy parameter has not been provided, remove any existing graylog package sources from the 01proxy file (if
+    # it exists) 
+    file_line { 'Remove graylog config from apt proxy file':
+      ensure   => absent,
+      path     => '/etc/apt/apt.conf.d/01proxy',
+      match    => 'graylog',
+      match_for_absence => true,
+      multiple => true,
+      before  => Apt::Source['graylog']
+    }
   }
 }
