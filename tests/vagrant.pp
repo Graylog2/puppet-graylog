f331d1967ac48d7212a928d7f0f1502a7d5f0950
Donald Morton <donald.morton@graylog.com>
2021-10-25 12:56:31 -0500

    Make puppet-lint and rubocop happy.

--- tests/vagrant.pp
+++ tests/vagrant.pp
@@ -9,8 +9,8 @@ class { 'graylog::allinone':
     version      => '7.10.2',
   },
   graylog       => {
-    major_version => '4.2',
-    config        => {
+    major_version          => '4.2',
+    config                 => {
       'password_secret'          => '16BKgz0Qelg6eFeJYh8lc4hWU1jJJmAgHlPEx6qkBa2cQQTUG300FYlPOEvXsOV4smzRtnwjHAKykE3NIWXbpL7yGLN7V2P2',
       'root_password_sha2'       => '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918',
       'versionchecks'            => false,
@@ -18,6 +18,6 @@ class { 'graylog::allinone':
       'http_bind_address'        => '0.0.0.0:9000',
     },
     java_initial_heap_size => '2g',
-    java_max_heap_size => '2g',
+    java_max_heap_size     => '2g',
   }
 }
