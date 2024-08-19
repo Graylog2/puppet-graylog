7eeb24358963e0e253e4a0405d0849d569276a81
Steven POST <steven.post@sfpd.fgov.be>
2024-08-19 12:35:52 +0200

    Mark config file as sensitive
    This file contains passwords and other secrets, treating it as sensitive
    prevents Puppet from showing the diff and thus leaking the password in
    reports.

--- manifests/server.pp
+++ manifests/server.pp
@@ -47,7 +47,7 @@ class graylog::server (
     owner   => $user,
     group   => $group,
     mode    => '0640',
-    content => template("${module_name}/server/graylog.conf.erb"),
+    content => Sensitive(template("${module_name}/server/graylog.conf.erb")),
   }
 
   case $::osfamily {
